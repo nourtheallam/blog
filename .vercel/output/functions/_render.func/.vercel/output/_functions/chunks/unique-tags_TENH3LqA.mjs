e59370d1c77fe498a18f1821488656a778943a1a
nallam <nr858090@dal.ca>
2025-01-10 00:16:57 -0400

    Resetting

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/unique-tags_TENH3LqA.mjs
@@ -0,0 +1,9 @@
+async function getMod() {
+						return import('./unique-tags_BshA7KXl.mjs');
+					}
+					const collectedLinks = [];
+					const collectedStyles = [];
+					const collectedScripts = [];
+					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
+
+export { defaultMod as default };
